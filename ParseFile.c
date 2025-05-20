55d1c24e7d36f847413d703f925ebf7bf3e38b33
Natalie Hoang <nlhoang1@uci.edu>
2025-05-19 17:00:08 -0700

    Create ParseFile.c

---
+++ ParseFile.c
@@ -0,0 +1,12 @@
+// ParseFile.c
+// This program parses inpuuted SRS file 
+#include "ParseFile.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <regex.h>
+
+// Stub for parse_srs function
+Requirement* parse_srs(const char *filename, int *req_count) {
+    // TODO: Implement markdown parsing
+    return NULL;
